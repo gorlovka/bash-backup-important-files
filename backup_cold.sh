26a2057f107a83df19029e4a0641aa57a94f33dc
Alex Lauer <contact@kagda.ru>
2022-02-16 00:10:39 +0300

    hot and cold splitting

--- backup_cold.sh
+++ backup_cold.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-#disk name
-COLD_DISK=/media/sh/250
+source  .env
 
 # destination folder
 COLD_DISK_FOLDER=$COLD_DISK/`date +%Y-%m-%d`
 
-# source folder for data
-ROOT_FOLDER=/home/sh
 
 mkdir $COLD_DISK_FOLDER || true > /dev/null
 
-cat list_files_hidden.txt | while read entity; do 
+cat list_files.txt | while read entity; do 
   FILENAME=$ROOT_FOLDER/$entity;
   echo $FILENAME 
-  rsync -rau -v  --ignore-existing   $FILENAME $COLD_DISK_FOLDER/  
+  rsync -v -u -a -L -r  --stats    $FILENAME $COLD_DISK_FOLDER/  
 done
--- backup_hot.sh
+++ backup_hot.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+source  .env
 
+# destination folder
+HOT_DISK_FOLDER=$HOT_DISK/`date +%Y-%m-%d`
+
+
+cat list_files.txt | while read entity; do 
+  FILENAME=$ROOT_FOLDER/$entity;
+  echo $FILENAME 
+  rsync -v -u -a -L -r --mkpath   --stats  -e "ssh -p $HOT_PORT"   $FILENAME $HOT_ADDRESS:$HOT_DISK_FOLDER/  
+done
