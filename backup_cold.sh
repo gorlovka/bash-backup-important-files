66bb1f276f9c1e232009eec2534b1bafd94a3d5d
Alex Sh <contact@kagda.ru>
2023-10-22 15:37:34 +0300

    local config

--- backup_cold.sh
+++ backup_cold.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+#EXAMPLE TO RUN
+#  bash backup_cold.sh list_files.txt
+#  bash backup_cold.sh list_files_hidden.txt
 
 source  .env
 
 # destination folder
 COLD_DISK_FOLDER=$COLD_DISK/`date +%Y-%m-%d`
 
+SOURCE_FILE=$1
+
 
 mkdir $COLD_DISK_FOLDER || true > /dev/null
 
-cat list_files.txt | while read entity; do 
+cat $SOURCE_FILE | while read entity; do 
   FILENAME=$ROOT_FOLDER/$entity;
   echo $FILENAME 
   rsync -v -u -a -L -r  --stats    $FILENAME $COLD_DISK_FOLDER/  
--- backup_hot.sh
+++ backup_hot.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+#EXAMPLE TO RUN
+#  bash backup_hot.sh list_files.txt
+#  bash backup_hot.sh list_files_hidden.txt
+
+
 source  .env
 
 # destination folder
 HOT_DISK_FOLDER=$HOT_DISK/`date +%Y-%m-%d`
 
+SOURCE_FILE=$1
 
-cat list_files.txt | while read entity; do 
+
+cat $SOURCE_FILE | while read entity; do 
   FILENAME=$ROOT_FOLDER/$entity;
   echo $FILENAME 
   rsync -v -u -a -L -r --mkpath   --stats  -e "ssh -p $HOT_PORT"   $FILENAME $HOT_ADDRESS:$HOT_DISK_FOLDER/  
